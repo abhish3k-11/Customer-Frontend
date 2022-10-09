57bcf1c1e442bc0f1b0f7228d95ee269377799f1
Abhishek Mishra <shani@DESKTOP-F413UGO.localdomain>
2022-10-09 16:51:19 +0530

    Creted Folder Struture

---
+++ src/Assets/index.js
@@ -0,0 +1,13 @@
+import { ReactComponent as BrandLogo } from "./BrandLogo.svg"
+import { ReactComponent as Information } from "./Info.svg"
+import { ReactComponent as Keyboard } from "./Keyboard.svg"
+import { ReactComponent as LoginIllustration } from "./LoginIllustration.svg"
+
+const Assets = {
+    BrandLogo,
+    Information,
+    Keyboard,
+    LoginIllustration,
+}
+
+export default Assets;
